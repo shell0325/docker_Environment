1a29412d3e27d182418b00bf7e8e25ce1feca77b
菅陽向 <syun@kanakiramukinoMacBook-Pro.local>
2021-08-21 23:01:17 +0900

    修正完了

---
+++ app/controller/login.controller.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const passport = require('passport');
+const validateLoginInput = require('./validation/login');
+
+exports.loginController = (req, res,next) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  passport.authenticate('login', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        res.render('login', {
+          errorMessage: ['このユーザーは存在しません'],
+        });
+      }
+      if (user.email !== req.body.email) {
+        res.render('login', {
+          errorMessage: ['ユーザー情報が間違っています'],
+        });
+      } else {
+        req.login(user, { session: false }, async (error) => {
+          if (error) {
+            return res.redirect('/login');
+          }
+          const body = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+          };
+          const token = jwt.sign(body, process.env.secretOrKey);
+          res.cookie('token', token, { httpOnly: true });
+          res.redirect('/');
+        });
+      }
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+};
--- app/routes/login.js
+++ app/routes/login.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const passport = require('passport');
+const UserController = require('../controller/login.controller');
 
 const validateLoginInput = require('../controller/validation/login');
 
@@ -10,43 +11,6 @@ router.get('/', (req, res, next) => {
   res.render('login.ejs');
 });
 
-router.post('/', async (req, res, next) => {
-  const { errors, isValid } = validateLoginInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  passport.authenticate('login', async (err, user, info) => {
-    try {
-      if (err || !user) {
-        res.render('login', {
-          errorMessage: ['このユーザーは存在しません'],
-        });
-      }
-      if (user.email !== req.body.email) {
-        res.render('login', {
-          errorMessage: ['ユーザー情報が間違っています'],
-        });
-      } else {
-        req.login(user, { session: false }, async (error) => {
-          if (error) {
-            return res.redirect('/login');
-          }
-          const body = {
-            id: user.id,
-            email: user.email,
-            username: user.username,
-          };
-          const token = jwt.sign(body, process.env.secretOrKey);
-          res.cookie('token', token, { httpOnly: true });
-          res.redirect('/');
-        });
-      }
-    } catch (error) {
-      return next(error);
-    }
-  })(req, res, next);
-});
+router.post('/', UserController.loginController);
 
 module.exports = router;
